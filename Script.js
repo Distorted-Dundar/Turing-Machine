351226a6a33a20688ef1d0224f5f57d3e5f0dcfb
B. Flores <56740397+Distorted-Dundar@users.noreply.github.com>
2022-11-18 12:53:56 -0500

    react test

--- Script.js
+++ Script.js
@@ -2,10 +2,11 @@
 function add(){
    // alert("blabla");
    var element = document.createElement("input");
-   element.maxlength = 1
+   // element.maxlength = 1
    element.size = "1"
    element.type = "text"
   // add("text");
+  element.setAttribute("maxlength", "1");
     var foo = document.getElementById("add1");
     //Append the element in page (in span).
     foo.appendChild(element);
